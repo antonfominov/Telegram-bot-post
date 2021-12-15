dc91723f332cb4cdbf27ee86938940089a88b5b0
antonfominov <61196201+antonfominov@users.noreply.github.com>
2021-12-15 23:13:10 +0300

    Возможность постинга в 3 канала

--- bot_v3.py
+++ bot_v3.py
@@ -10,31 +10,44 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-API_TOKEN = config.get('DEFAULT', 'API_TOKEN')
-CHAT_ID = config["DEFAULT"]["CHAT_ID"]
-PATH = config["DEFAULT"]["PATH"]
+API_TOKEN = config["DEFAULT"]["API_TOKEN"]
+
+FAKEHUB_ID = config["HUB"]["CHAT_ID"]
+FAKEHUB_PATH = config["HUB"]["PATH"]
+
+NINJAS_ID = config["NINJA"]["CHAT_ID"] 
+NINJAS_PATH = config["NINJA"]["PATH"]
+
+SILENTGIRL_ID = config["GIRL"]["CHAT_ID"]
+SILENTGIRL_PATH = config["GIRL"]["PATH"]
 
 logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-x=11
+x=0
 
-async def add_photo(time):
-    filename_list = glob.glob(PATH)
+async def add_photo(channel, path):
+    filename_list = glob.glob(path)
     filename = filename_list[0]
-    photo = open(filename, 'rb')
+    file = open(filename, 'rb')
+    
+    if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+        await bot.send_photo(channel, file)
+    elif filename.lower().endswith('.gif'):
+        await bot.send_animation(channel, file)
+    
     logging.info('Пост опубликован')
-    await bot.send_photo(CHAT_ID, photo)
-    photo.close()
+    
+    file.close()
     os.remove(filename)
 
 async def add_gif(time):
-    filename_list = glob.glob(PATH)
+    filename_list = glob.glob(path)
     filename = filename_list[0]
     photo = open(filename, 'rb')
     logging.info('Пост опубликован')
-    await bot.send_animation(CHAT_ID, photo, caption=time)
+    await bot.send_animation(FAKEHUB_ID, photo, caption=time)
     photo.close()
     os.remove(filename)
     
@@ -42,35 +55,70 @@ async def start_post(x):
     print('Цикл начался')
     logging.info('Цикл начался')
     while True:
-        time = datetime.now()
-        if time.hour==7 and x==0:
-            x=7
-            await asyncio.create_task(add_photo(time))
-        elif time.hour==9 and x==7:
-            x=9
-            await asyncio.create_task(add_photo(time))
-        elif time.hour==11 and x==9:
-            x=11
-            await asyncio.create_task(add_photo(time))
-        elif time.hour==15 and x==11:
-            x=15
-            await asyncio.create_task(add_photo(time))
-        elif time.hour==17 and x==15:
-            x=17
-            await asyncio.create_task(add_photo(time))
-        elif time.hour==19 and x==17:
-            x=19
-            await asyncio.create_task(add_photo(time))
-        elif time.hour==21 and x==19:
-            x=21
-            await asyncio.create_task(add_photo(time))
-        elif time.hour==23 and x==21:
-            x=0
-            await asyncio.create_task(add_photo(time))
+        time = datetime.now().hour
+        if time==6 and x==6:
+            x+=1
+            await asyncio.create_task(add_photo(NINJAS_ID, NINJAS_PATH))
+            await asyncio.create_task(add_photo(SILENTGIRL_ID, SILENTGIRL_PATH))
+        elif time==7 and x==7:
+            x+=1
+            await asyncio.create_task(add_photo(FAKEHUB_ID, FAKEHUB_PATH))
+        elif time==8 and x==8:
+            x+=1
+            await asyncio.create_task(add_photo(NINJAS_ID, NINJAS_PATH))
+            await asyncio.create_task(add_photo(SILENTGIRL_ID, SILENTGIRL_PATH))
+        elif time==9 and x==9:
+            x+=1
+            await asyncio.create_task(add_photo(FAKEHUB_ID, FAKEHUB_PATH))
+        elif time==10 and x==10:
+            x+=1
+            await asyncio.create_task(add_photo(NINJAS_ID, NINJAS_PATH))
+            await asyncio.create_task(add_photo(SILENTGIRL_ID, SILENTGIRL_PATH))
+        elif time==11 and x==11:
+            x=14
+            await asyncio.create_task(add_photo(FAKEHUB_ID, FAKEHUB_PATH))
+        elif time==14 and x==14:
+            x+=1
+            await asyncio.create_task(add_photo(NINJAS_ID, NINJAS_PATH))
+            await asyncio.create_task(add_photo(SILENTGIRL_ID, SILENTGIRL_PATH))
+        elif time==15 and x==15:
+            x+=1
+            await asyncio.create_task(add_photo(FAKEHUB_ID, FAKEHUB_PATH))
+        elif time==16 and x==16:
+            x+=1
+            await asyncio.create_task(add_photo(NINJAS_ID, NINJAS_PATH))
+            await asyncio.create_task(add_photo(SILENTGIRL_ID, SILENTGIRL_PATH))
+        elif time==17 and x==17:
+            x+=1
+            await asyncio.create_task(add_photo(FAKEHUB_ID, FAKEHUB_PATH))
+        elif time==18 and x==18:
+            x+=1
+            await asyncio.create_task(add_photo(NINJAS_ID, NINJAS_PATH))
+            await asyncio.create_task(add_photo(SILENTGIRL_ID, SILENTGIRL_PATH))
+        elif time==19 and x==19:
+            x+=1
+            await asyncio.create_task(add_photo(FAKEHUB_ID, FAKEHUB_PATH))
+        elif time==20 and x==20:
+            x+=1
+            await asyncio.create_task(add_photo(NINJAS_ID, NINJAS_PATH))
+            await asyncio.create_task(add_photo(SILENTGIRL_ID, SILENTGIRL_PATH))
+        elif time==21 and x==21:
+            x+=1
+            await asyncio.create_task(add_photo(FAKEHUB_ID, FAKEHUB_PATH))
+        elif time==22 and x==22:
+            x+=1
+            await asyncio.create_task(add_photo(NINJAS_ID, NINJAS_PATH))
+            await asyncio.create_task(add_photo(SILENTGIRL_ID, SILENTGIRL_PATH))
+        elif time==23 and x==23:
+            x=6
+            await asyncio.create_task(add_photo(FAKEHUB_ID, FAKEHUB_PATH))
         await asyncio.sleep(1200) #Работает! Можно попасть в бесконечный цикл
         
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
+    if 23 < datetime.now().hour < 6:
+        x = 6
+    else: x = datetime.now().hour
     asyncio.create_task(start_post(x))
     print('Начало постинга')
     logging.info('Постинг начался')
@@ -79,7 +127,7 @@ async def send_welcome(message: types.Message):
 @dp.message_handler(commands=['status'])
 async def send_status(message: types.Message):
     logging.info(message)
-    await message.reply(f'Я всё еще жив! {datetime.now().hour}')
+    await message.reply(f'Я всё еще жив! Время: {datetime.now().hour}, x:{x}')
 
 @dp.message_handler(commands=['test'])
 async def send_test(message: types.Message):
